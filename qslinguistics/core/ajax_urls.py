a87a2c4d5ca77df94a811f05ad9a6a39a10043d8
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-09-11 19:48:17 +0800

    Added ajax_urls.py

---
+++ qslinguistics/core/ajax_urls.py
@@ -0,0 +1,43 @@
+import json
+import aiohttp
+from typing import Dict
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+
+@dataclass
+class AJAXURL:
+    """
+    The AJAXURL object finds the AJAX URL that stores data.
+    """
+
+    year: int
+
+    def find_ajax_url(self, soup: BeautifulSoup) -> str:
+        """The find_url method finds the AJAX URL.
+
+        Args:
+            soup (BeautifulSoup): the BeautifulSoup object
+
+        Returns:
+            a string (i.e. the URL)
+        """
+        jsonified_ajax = json.loads(
+            soup.find("script", {"type": "application/json"}).string
+        )
+        return jsonified_ajax["qs_rankings_datatables"]["rank_url"]
+
+    async def get_ajax_url(self) -> str:
+        """The get_ajax_url method gets the AJAX URL.
+
+        Returns:
+            a str (i.e. the URL)
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://www.topuniversities.com/university-rankings/university-subject-rankings/{self.year}/linguistics"
+            ) as response:
+                html = await response.text()
+                soup = BeautifulSoup(html, "lxml")
+                url = self.find_ajax_url(soup)
+                return url
